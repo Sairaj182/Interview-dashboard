2a9a12f4008e57dd768852c58c45f2e276e9a292
Sairaj182 <sairajraithatha.official@gmail.com>
2026-03-15 06:11:51 +0530

    Bug fix

--- src/lib/db.js
+++ src/lib/db.js
@@ -10,6 +10,7 @@ if (!global.sequelize) {
     process.env.DB_PASS,
     {
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       dialect: "mysql",
       dialectModule: require("mysql2"),
       logging: false
